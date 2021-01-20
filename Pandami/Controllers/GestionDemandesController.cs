e02b9c087c258eb5cb306a6f9aa9551275e5fc6a
Mathilda <mathilda.landreau@wanadoo.fr>
2021-01-20 16:40:08 +0100

    Ajoutez des fichiers projet.

---
+++ Pandami/Controllers/GestionDemandesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pandami.Controllers
+{
+    public class GestionDemandesController : Controller
+    {
+        // GET: GestionDemandes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
